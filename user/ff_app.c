66086b60632ad5071be3b5a67fdcc70be1ac0f39
jibingrg <jibingrg12@gmail.com>
2021-06-11 16:59:34 +0530

    FFApp updated
    Video pixel height and width are set while initializing FFE. FFApp updated accordingly.

--- user/ff_app.c
+++ user/ff_app.c
@@ -9,8 +9,9 @@
 
 #define MAX_WIDTH		3840
 #define MAX_HEIGHT		2160
-#define MAX_FPS			120
+#define MAX_FPS			125
 #define FPS_DEFAULT		25
+#define FCOUNT_MIN		40
 #define FCOUNT_DEFAULT		120
 
 #define COLOR_WHITE		{ 0xFF, 0xFF, 0xFF}
@@ -29,38 +30,127 @@ static const unsigned char bar[8][3] = {
 struct ffe_data {
 	int framerate;
 	int framecount;
+	int width;
+	int height;
 } data;
 
-void ff_init(void)
+char gst[200];
+
+int str_to_int(char *p)
 {
-	int fd1, fd2, i, j, cmd;
-	unsigned int bytesperline = 2 * MAX_WIDTH;
-	unsigned char *buf = malloc(bytesperline);
+	int i, ret = 0;
+
+	for (i = 0; p[i]; i++) {
+		if ((p[i] > '9') || (p[i] < '0')) {
+			printf("invalid argument..\n");
+			return 0;
+		}
+		ret = ret * 10 + p[i] - '0';
+	}
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+	int i, j, k;
+	int fract;
+	unsigned int size1, size2;
+
+	if (argc == 2) {
+		char gst1[] = "gst-launch-1.0 v4l2src device=";
+		char gst2[] = " ! videoconvert ! autovideosink";
+
+		strcat(gst, gst1);
+		strcat(gst, argv[1]);
+		strcat(gst, gst2);
+
+		printf("\n");
+		for (i = 0; gst[i]; i++)
+			printf("%c", gst[i]);
+		printf("\n\n");
+
+		system(gst);
+		return 0;
+	}
+
+	if (argc != 5) {
+		printf("Expecting command line arguments..\n");
+		printf("sudo ./FFApp <I/G> <frame_rate> <frame_count> <pixel_height>\n");
+		return -1;
+	}
 
 	fd1 = open("/dev/FFE", O_RDWR);
 	if (fd1 < 0) {
 		printf("FFE open error..\n");
-		return;
+		return -1;
 	}
 
-	printf("\t0\tFeed frames from a file\n\t1\tGenerate colorbar\n Select an option: ");
-	scanf("%d", &cmd);
-
-	printf("Enter framerate: ");
-	scanf("%d", &data.framerate);
-
+	data.framerate = str_to_int(argv[2]);
 	if ((!data.framerate) || (data.framerate > MAX_FPS))
 		data.framerate = FPS_DEFAULT;
 
-	if (cmd) {
-		int flag;
-		int start, end;
-		unsigned char yuv[8][3];
-		unsigned char colorbar[MAX_WIDTH * 4], *p;
-
+	data.framecount = str_to_int(argv[3]);
+	if (data.framecount < FCOUNT_MIN)
 		data.framecount = FCOUNT_DEFAULT;
-		printf("\nframe rate = %d\tframe count = %d\n", data.framerate, data.framecount);
-		ioctl(fd1, 0, &data);
+
+	data.height = str_to_int(argv[4]);
+	if (data.height <= 270)
+		data.height = 270;
+	else if (data.height <= 360)
+		data.height = 360;
+	else if (data.height <= 720)
+		data.height = 720;
+	else if (data.height <= 1080)
+		data.height = 1080;
+	else
+		data.height = 2160;
+
+	data.width = data.height * 16 / 9;
+	ioctl(fd1, 0, &data);
+	size1 = MAX_WIDTH << 1;
+	size2 = data.width << 1;
+	fract = MAX_WIDTH / data.width;
+
+	printf("%s: frame rate = %d, frame count = %d\n", __func__, data.framerate, data.framecount);
+	printf("%s: frame width = %d, frame height = %d\n", __func__, data.width, data.height);
+
+	if (argv[1][0] == 'I') {
+		unsigned char *buf1, *buf2;
+
+		fd2 = open("sample/video_3840x2160.yuv", O_RDONLY);
+		if (fd2 < 0) {
+			printf("Sample file open error..\n");
+			close(fd1);
+			return -1;
+		}
+
+		buf1 = malloc(size1);
+		buf2 = malloc(size2);
+
+		for (i = 0; i < data.framecount; i++) {
+			for (j = 0; j < MAX_HEIGHT; j++) {
+				unsigned char *p = buf2;
+
+				read(fd2, buf1, size1);
+				if (j % fract)
+					continue;
+				for (k = 0; k < size1; k += (fract << 2)) {
+					memcpy(p, buf1 + k, 4);
+					p += 4;
+				}
+				write(fd1, buf2, size2);
+			}
+		}
+
+		free(buf1);
+		free(buf2);
+		close(fd2);
+	} else if (argv[1][0] == 'G') {
+		int flag, start, end;
+		unsigned char yuv[8][3];
+		unsigned char colorbar[data.width << 2];
+		unsigned char *p;
 
 		for (i = 0; i < 8; i++) {
 			yuv[i][0] = (((16829 * bar[i][0] + 33039 * bar[i][1] + 6416 * bar[i][2]  + 32768) >> 16) + 16);
@@ -69,10 +159,10 @@ void ff_init(void)
 		}
 
 		for (i = 0; i < 16; i++) {
-			start = i * MAX_WIDTH / 8;
-			end = (i + 1) * MAX_WIDTH / 8;
+			start = i * data.width >> 3;
+			end = (i + 1) * data.width >> 3;
 			flag = 1;
-			p = &colorbar[start * 2];
+			p = &colorbar[start << 1];
 
 			for (j = start; j < end; j++) {
 				*p = yuv[i % 8][0];
@@ -82,105 +172,22 @@ void ff_init(void)
 			}
 		}
 
-		for (i = 0; i < FCOUNT_DEFAULT; i++) {
-			p = colorbar + ((i * MAX_WIDTH / FCOUNT_DEFAULT) % MAX_WIDTH) * 2;
-			memcpy(buf, p, bytesperline);
-			for (j = 0; j < MAX_HEIGHT; j++)
-				write(fd1, buf, bytesperline);
+		for (i = 0; i < data.framecount; i++) {
+			p = colorbar + (((i * data.width / data.framecount) % data.width) << 1);
+			for (j = 0; j < data.height; j++)
+				write(fd1, p, size2);
 		}
 	} else {
-		printf("Enter framecount: ");
-		scanf("%d", &data.framecount);
-
-		if ((!data.framecount) || (data.framecount > FCOUNT_DEFAULT))
-			data.framecount = FCOUNT_DEFAULT;
-
-		printf("\nframe rate = %d\tframe count = %d\n", data.framerate, data.framecount);
-		ioctl(fd1, 0, &data);
-
-		fd2 = open("sample/video_3840x2160.yuv", O_RDONLY);
-		if (fd2 < 0) {
-			printf("Sample file open error..\n");
-			close(fd1);
-			free(buf);
-			return;
-		}
-
-		for (i = 0; i < data.framecount; i++) {
-			for (j = 0; j < MAX_HEIGHT; j++) {
-				read(fd2, buf, bytesperline);
-				write(fd1, buf, bytesperline);
-			}
-		}
-
-		close(fd2);
+		printf("Expeting second argument as\n\t'I' to insert frames from sample file, or\n\t'G' to generate frames");
+		close(fd1);
+		return -1;
 	}
 
 	printf("\nTotal %d frames are inserted\n", i);
 	ioctl(fd1, 1, 0);
 	close(fd1);
-	free(buf);
-}
 
-int main(int argc, char *argv[])
-{
-	int cmd, vsize, i;
-	char gst[200], device[20];
-	char gst1[] = "gst-launch-1.0 v4l2src device=";
-	char gst2[] = " ! video/x-raw,interlace-mode=interleaved,";
-	char gst3[] = " ! videoconvert ! videoscale ! autovideosink";
-	char size[5][25] = {
-		"width=480,height=270", "width=640,height=360", "width=1280,height=720", "width=1920,height=1080", "width=3840,height=2160"
-	};
-
-	if (argc == 1) {
-		printf("Enter video device node path: ");
-		scanf("%s", device);
-	} else {
-		strcpy(device, argv[1]);
-	}
-
-	while (1) {
-		printf("\t0\tExit\n\t1\tInitialize FFE\n\t2\tStream video\nSelect your option: ");
-		scanf("%d", &cmd);
-
-		if (!cmd)
-			break;
-
-		if (cmd == 1) {
-			printf("******************************************************\n");
-			ff_init();
-			printf("******************************************************\n");
-		} else if (cmd == 2) {
-			printf("******************************************************\n");
-			printf("\t0\t480x270\n\t1\t640x360\n\t2\t1280x720\n\t3\t1920x1080\n\t4\t3840x2160\nSelect video size: ");
-			scanf("%d", &vsize);
-
-			if ((vsize < 0) || (vsize > 4)) {
-				printf("Selected option is not valid..\n");
-				break;
-			}
-
-			for (i = 0; i < 200; i++)
-				gst[i] = 0;
-
-			strcat(gst, gst1);
-			strcat(gst, device);
-			strcat(gst, gst2);
-			strcat(gst, size[vsize]);
-			strcat(gst, gst3);
-			printf("\n");
-			for (i = 0; gst[i]; i++)
-				printf("%c", gst[i]);
-			printf("\n");
-			printf("******************************************************\n");
-			system(gst);
-			printf("******************************************************\n");
-		} else {
-			printf("******************************************************\n");
-			printf("Enter a valid command:\n");
-		}
-	}
-
+	printf("\nRun FFApp with video node as commandline argument to stream video\n");
+	printf("./FFApp <video device node>\n");
 	return 0;
 }
